18fb4edf94d6eab4d4ad78bc601abb471d3cd99b
berna39 <kalemajoseph39@gmail.com>
2022-04-29 17:15:21 +0200

    Rootings essentails

--- server.js
+++ server.js
@@ -5,3 +5,31 @@ const app = express();
 app.listen(3000, ()=>{
     console.log('App started');
 });
+
+//for files
+app.get('/file.txt', function(req, res){
+    res.send('file.txt');
+});
+
+// routing bases on masks
+
+// b? means optional character
+app.get('/ab?cd', function(req, res){
+    res.send('ab?cd');
+});
+
+
+// starts by ab and ends with cd
+app.get('/ab*cd', function(req, res){
+    res.send('ab*cd'); 
+});
+
+// here the client can send abcd, abbbbbbcd, abbbbbbbbcd, etc;
+app.get('/ab+cd', function(req, res){
+    res.send('ab+cd'); 
+});
+
+// here cd is optional
+app.get('/ab(cd)?e', function(req, res) {
+    res.send('ab(cd)?e');
+});
